63f8a1b7e8c1bf4d87c5fa57b5683df02982f7b6
emiliaosk <81231647+emiliaosk@users.noreply.github.com>
2021-03-27 17:12:02 +0000

    prufa

--- src/myProject/tripController.java
+++ src/myProject/tripController.java
@@ -1,6 +1,26 @@
 package myProject;
 
-public class tripController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class tripController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        @FXML
+        private Button addButton;
+        @FXML
+        private ListView maxReservations;
+        @FXML
+    }
+}
+
+/*public class tripController {
     public Boolean cancelTrip(Boolean b) {
         if (b = true) {
             return true;
