5a66c62a1fdf5638f260def09c5364fa0190b0b9
rthb2 <80471217+rthb2@users.noreply.github.com>
2021-04-10 16:06:19 +0000

    Search

--- src/myProject/Search.java
+++ src/myProject/Search.java
@@ -3,19 +3,35 @@ package myProject;
 public class Search {
 
     public String searchDestination(String dest) {
-        dest = trip.getDestination;
+        dest = trip.getDestination();
+        if (trip.isFullyBooked() == true)
+            return null;
+        if (!searchDestination(dest))
+            return null;
         return dest;
     }
 
-    public String searchCategory() {
-
+    public String searchCategory(String category) {
+        category = trip.getCategory();
+        return category;
+        if (!searchCategory(category)) {
+            return null;
+        }
     }
 
-    public float searchDate() {
-
+    public float searchDate(float date) {
+        date = trip.getDate();
+        return date;
+        if (!searchDate(date)) {
+            return null;
+        }
     }
 
-    public flot searchTime() {
-
+    public flot searchTime(float time) {
+        time = trip.getTime();
+        return time;
+        if (!searchTime(time)) {
+            return null;
+        }
     }
 }
--- src/myProject/searchController.java
+++ src/myProject/searchController.java
@@ -2,5 +2,4 @@ package myProject;
 
 public class searchController {
 
-
 }
