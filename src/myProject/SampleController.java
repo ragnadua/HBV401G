c2f8874c79da1332539bf109047a2813a243f2fe
ernagudlaug <egg25@hi.is>
2021-04-19 15:49:18 +0000

    scene builder egg

--- src/myProject/SampleController.java
+++ src/myProject/SampleController.java
@@ -1,5 +1,35 @@
 package myProject;
 
-public class SampleController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SampleController implements Initializable {
+    @FXML
+    private Button addButton;
+    @FXML
+    private ListView usersListView;
+    @FXML
+    private ListView reservationsListView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField emailTextField;
+
+    public Database database = new Database.getInstance();
+
+    private ObservableList<Account> accounts = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        accounts = database.getaccounts();
+        usersListView.setItems(accounts);
+    }
 }
