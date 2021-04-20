69b935e4293282821b3ba830ce52ec2462df7e3e
ragnadua <80420790+ragnadua@users.noreply.github.com>
2021-04-20 11:18:17 +0000

    eftir consult

--- src/myProject/Main.java
+++ src/myProject/Main.java
@@ -1,23 +1,7 @@
 package myProject;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("myProject.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+public class Main {
+    public void run(String[] args) {
 
     }
 }
